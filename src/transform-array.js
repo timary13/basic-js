5878ca378f72f32e8af55e3c9395c3051e4c7b57
Marina Gordynets <timary99@gmail.com>
2020-10-05 00:02:04 +0300

    fix: add right solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,25 +5,28 @@ module.exports = function transform(arr) {
   for(let i = 0; i < arr.length; i++) {
     switch (arr[i]) {
       case '--discard-next':
-        i += 2;
+        i += 1;
         break;
       case '--discard-prev':
-        res.pop();
+        if(arr[i - 2] !== '--discard-next') {
+          const prevArr = arr[i - 1];
+          if(res.includes(prevArr)) res.pop();
+        }
         break;
       case '--double-next':
         const next = arr[i + 1];
         if(next !== undefined) res.push(next);
         break;
       case '--double-prev':
-        const prev = arr[i - 1];
-        if(prev !== undefined) res.push(prev);
+        if(arr[i - 2] !== '--discard-next') {
+          const prev = arr[i - 1];
+          if(res.includes(prev)) res.push(prev);
+        }
         break;
       default:
         res.push(arr[i]);
         break;
     }
-    // console.log(res, i);
   }
-  console.log(res);
   return res;
 };
